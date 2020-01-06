31bb12882908dfdc547df554fb6d73d65c0e9a03
ParkChan <hydrarkk@naver.com>
2020-01-07 00:48:18 +0900

    [작업내역]

--- app/src/main/java/com/example/githubsearcher/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/githubsearcher/ui/home/HomeFragment.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.githubsearcher.R
+import com.google.android.material.textfield.TextInputEditText
 
 class HomeFragment : Fragment() {
 
@@ -22,10 +21,7 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+        val inputText: TextInputEditText = root.findViewById(R.id.et_fragment_home_keyword)
         return root
     }
 }
