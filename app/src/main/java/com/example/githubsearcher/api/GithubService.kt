c73d85966221d58b7058f05d0c131a6bfd70b3f6
ParkChan <hydrarkk@naver.com>
2020-02-18 00:30:15 +0900

    [작업내역] 인터페이스 수정

--- app/src/main/java/com/example/githubsearcher/api/GithubService.kt
+++ app/src/main/java/com/example/githubsearcher/api/GithubService.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2018 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.githubsearcher.api
 
 import com.example.githubsearcher.tapmenu.home.model.ResGithubInfo
@@ -30,7 +14,6 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 private const val TAG = "GithubService"
-private const val IN_QUALIFIER = "in:name,description"
 
 fun searchRepos(
     service: GithubService,
@@ -66,13 +49,7 @@ fun searchRepos(
     )
 }
 
-/**
- * Github API communication setup via Retrofit.
- */
 interface GithubService {
-    /**
-     * Get responseData ordered by stars.
-     */
     @GET("search/repositories?sort=stars")
     fun searchRepos(
         @Query("q") query: String,
